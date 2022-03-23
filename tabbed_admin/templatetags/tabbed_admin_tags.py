d1c2d9fb0c24c4c66b72ecb2b2cb4c1731c69e61
sasierra98 <83804697+sasierra98@users.noreply.github.com>
2022-03-23 16:36:10 -0500

    Update tabbed_admin_tags.py
    [BUG FIX] Add permission

--- tabbed_admin/templatetags/tabbed_admin_tags.py
+++ tabbed_admin/templatetags/tabbed_admin_tags.py
@@ -22,7 +22,8 @@ def render_tab_fieldsets_inlines(context, entry):
     request = context['request']
     obj = context.get('original', None)
     # readonly_fields = admin_form.model_admin.get_readonly_fields(request, obj)
-    if not admin_form.model_admin.has_change_permission(request, obj):
+    if not admin_form.model_admin.has_change_permission(request, obj) \
+            and not admin_form.model_admin.has_add_permission(request, obj):
         readonly_fields = list(admin_form.form.all_fields.keys())
     else:
         readonly_fields = admin_form.model_admin.get_readonly_fields(request, obj)
